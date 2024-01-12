75b76cabb031ecaacde3c927eea35dff856d39c3
Daniel Mera <daniel.mera@epn.edu.ec>
2024-01-11 21:47:25 -0500

    fix: metodo validar numero

--- codigo/assets/js/funciones.js
+++ codigo/assets/js/funciones.js
@@ -1,5 +1,5 @@
 function validarNumero(input) {
-    var regex = /[^0-9]/g;
+    var regex = /[^0-9.]/g;
     input.value = input.value.replace(regex, '');
     if (input.value.length === 0) {
         document.getElementById('mensaje').innerHTML = "Ingresa al menos un número.";
